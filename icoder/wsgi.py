51bee83fa45cddeda9fb06642b8397568709febe
harshbhatnagar-dotcom <harsh210806@gmail.com>
2025-07-07 13:16:14 +0530

    push

--- icoder/wsgi.py
+++ icoder/wsgi.py
@@ -16,7 +16,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'icoder.settings')  # Replace with your actual settings module
 
 # Initialize Django before calling management commands
-django.setup()
+
 
 # Auto-run `collectstatic` only if staticfiles dir is empty (safe for Render Free Tier)
 static_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'staticfiles')
@@ -24,10 +24,6 @@ if not os.path.exists(static_dir) or not os.listdir(static_dir):
     subprocess.call(['python', 'manage.py', 'collectstatic', '--noinput'])
 
 # Auto-run `migrate` on every start
-try:
-    subprocess.call(['python', 'manage.py', 'migrate'])
-except Exception as e:
-    print("Migration failed:", e)
 
 # Get the actual WSGI application
 application = get_wsgi_application()
